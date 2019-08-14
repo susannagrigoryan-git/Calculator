65a4de6476df0e8566aa346453c1991b8c974b76
ISTC <ISTC@STUDY-1-6.istc.com>
2019-08-14 11:33:10 +0400

    third commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const numberInputs = document.getElementsByClassName('number-input');
 const resultElement = document.getElementsByClassName('result')[0];
 const selectElement = document.getElementsByTagName('select')[0];
- const operation = document.getElementsByClassName('options')[0]
+ const operation = document.getElementsByClassName('text-input')[0]
 
 
 function clickHandler(){
@@ -9,7 +9,7 @@ function clickHandler(){
   const num1 = Number(numberInputs[0].value);
   const num2 = Number(numberInputs[1].value);
 
-var result = null
+let result = null
 switch(operation.value){
     case '+':
     result = num1 + num2; 
